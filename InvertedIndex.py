266401a882a2307234f92500dcfa0e4700f4940d
valeriy <v.gerasimov@innopolis.ru>
2017-09-10 23:28:15 +0300

    init commit

--- InvertedIndex.py
+++ InvertedIndex.py
@@ -7,7 +7,7 @@ class InvertedIndex():
         new_token = new_token.lower()
         if new_token not in self._tokens:
             self._tokens[new_token] = []
-        if (len(self._tokens[new_token]) == 0) or (self._tokens[new_token][len(self._tokens[new_token])-1] != doc_number) :
+        if (len(self._tokens[new_token]) == 0) or (self._tokens[new_token][-1] != doc_number):
             self._tokens[new_token].append(doc_number)
 
     def add_list(self, new_tokens, doc_number):
